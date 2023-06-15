9884624ffb585c0132d29f63cab79f55ec3b720e
Anastasia <teterina.anastasia@gmail.com>
2023-06-15 14:33:43 -0700

    revision

--- diversity_stats/Ce_Cr_plot_SMCPP.R
+++ diversity_stats/Ce_Cr_plot_SMCPP.R
@@ -122,8 +122,8 @@ shading <-data.frame(Species=c("C. elegans", "C. elegans", "C. remanei", "C. rem
                      xmin=c(0, 34000*4, 0, 1400000*4), xmax=c(100,Inf,100,Inf),ymin=c(0,0,0,0), ymax=c(Inf, Inf, Inf, Inf))
 
 
-FIG6B<-ggplot(SMC, aes(x = x, y = y, color= Species, fill=Species, ordered = FALSE))  +
-  labs(title ="B", x = "Generations", y = "Effective population size") +
+FIG5<-ggplot(SMC, aes(x = x, y = y, color= Species, fill=Species, ordered = FALSE))  +
+  labs( x = "Generations", y = "Effective population size") +
   scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),labels = trans_format(trans="log10", math_format(10^.x))) +
   scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),labels = trans_format(trans="log10", math_format(10^.x)),limits = c(1,0.5e7)) +
   theme_bw(base_size = 12) +
@@ -139,86 +139,6 @@ FIG6B<-ggplot(SMC, aes(x = x, y = y, color= Species, fill=Species, ordered = FAL
 
 
 
-###GONE results
-my_files2<-list.files(pattern="Output_Ne_")
-my_names2<-gsub("Output_Ne_","", perl=T, my_files2)
-my_names2<-gsub("_data_rec","", perl=T, my_names2)
-
-GONE <-c();
-for (i in 1:(length(my_files2))) {
-  B = read.csv(file = my_files2[i],
-               header = TRUE,
-               sep = "\t", skip = 1)
-  GONE <- rbind(GONE, data.frame(B, sample = my_names2[i]))
-}
-
-GONE$Species<-gsub("CE","C. elegans",perl=TRUE,GONE$sample)
-GONE$Species<-gsub("CR","C. remanei",perl=TRUE,GONE$Species)
-
-
-#!!!!!read the discussion here https://github.com/popgenmethods/smcpp/issues/82
-#GONE[GONE$Species=="C. elegans",]$Generation[1] <-0
-GONE[GONE$Species=="C. elegans",]$Generation<- GONE[GONE$Species=="C. elegans",]$Generation*2
-GONE[GONE$Species=="C. elegans",]$Generation[1] <-1
-GONE[GONE$Species=="C. elegans",]$Geometric_mean <-GONE[GONE$Species=="C. elegans",]$Geometric_mean/2
-
-FIG6A<-ggplot(GONE, aes(x = Generation, y = Geometric_mean, color= Species, fill=Species, ordered = FALSE))  +
-  labs(title ="A", x = "Generations", y = "Effective population size") +
-  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),labels = trans_format(trans="log10", math_format(10^.x)),limits = c(1,0.5e7)) +
-  theme_bw(base_size = 12) +
-  theme(axis.text.x = element_text(angle=0,vjust=0.5, hjust = 0.5), plot.title = element_text(hjust = 0, face = "bold"),strip.background = element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank(), strip.text = element_text(size = rel(1))) +
-  theme(legend.position = "none") +
-  theme(strip.text.y = element_text(face = "italic"))+
-  theme(strip.background = element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank(), strip.text = element_text(size = rel(1))) +
-  facet_grid(Species~ .) +
-  geom_line(aes(group=sample),size=1.1, alpha=1) + xlim(c(0,501)) +
-  theme(plot.margin = unit(c(1, 3, 1.5, 3), "pt")) +
-  scale_color_manual(values = c("#196770","#196770"),name="") + scale_fill_manual(values = c("#225D5B","#225D5B"),name="")
-
-grid.arrange(FIG6A,FIG6B, nrow=1,widths=c(1,2)) ->F5
 tiff(filename = "Fig5.tiff",width = 7.2,height = 3.8,res=300,units="in")
-plot(F5)
+plot(FIG5)
 dev.off()
-
-
-####estimetion off the "recent Ne" by averagingthe last 100 generations
-
-
-
-
-
-
-resultscrGONE <- boot(data=GONE[GONE$Species=="C. remanei" & GONE$Generation<=100, ]$Geometric_mean, statistic=Bmean, R=1000)
-
-# view results
-resultscrGONE
-
-#ORDINARY NONPARAMETRIC BOOTSTRAP
-#
-#
-#Call:
-#  boot(data = GONE[GONE$Species == "C. remanei" & GONE$Generation <=
-#                     100, ]$Geometric_mean, statistic = Bmean, R = 1000)
-#
-#
-#Bootstrap Statistics :
-#  original    bias    std. error
-#t1* 116133.9 -238.9152    9047.852
-
-
-resultsceGONE <- boot(data=GONE[GONE$Species=="C. elegans" & GONE$Generation<=100, ]$Geometric_mean, statistic=Bmean, R=1000)
-
-# view results
-resultsceGONE
-
-#ORDINARY NONPARAMETRIC BOOTSTRAP
-#
-#
-#Call:
-#  boot(data = GONE[GONE$Species == "C. elegans" & GONE$Generation <=
-#                     100, ]$Geometric_mean, statistic = Bmean, R = 1000)
-#
-#
-#Bootstrap Statistics :
-#  original  bias    std. error
-#t1* 911025.3 -864.3987    51324.62
